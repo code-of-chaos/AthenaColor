17ac88390992b140c160496da8d2fce7668523b1
DirectiveAthena <directive.athena@gmail.com>
2022-03-22 18:16:09 +0100

    Change: Updated links

--- setup.py
+++ setup.py
@@ -14,15 +14,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="AthenaColor",
-    version="3.0.2",
+    version="3.0.3",
     author="Andreas Sas",
     author_email="",
     description="Python Package used to print rgb colors to the console",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/DirectiveAthena/AthenaColor",
+    url="https://github.com/DirectiveAthena/VSC-AthenaColor",
     project_urls={
-        "Bug Tracker": "https://github.com/DirectiveAthena/AthenaColor/issues",
+        "Bug Tracker": "https://github.com/DirectiveAthena/VSC-AthenaColor/issues",
     },
     license="GPLv3",
     package_dir={"": "src"},
