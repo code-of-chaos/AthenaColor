28693d1a2cd7952af2b6f80ef447a39578b5d7bc
DirectiveAthena <directive.athena@gmail.com>
2022-03-23 07:18:00 +0100

    Format: Spacing of Hexadecimal parsing

--- src/AthenaColor/Objects/Hexadecimal.py
+++ src/AthenaColor/Objects/Hexadecimal.py
@@ -19,6 +19,9 @@ class hexadecimal(rgb):
         if isinstance(hex_value, str) and hex_value[0] == "#":
             hex_v = hex_value.removeprefix('#')
             indexes = (0, 2, 4)
-            self.r,self.g, self.b = tuple(int(hex_v[i:i+2], 16) for i in indexes)
+            self.r,self.g, self.b = tuple(
+                int(hex_v[i:i+2], 16)
+                for i in indexes
+            )
         else:
             raise ValueError("no correct str was given on creation")
