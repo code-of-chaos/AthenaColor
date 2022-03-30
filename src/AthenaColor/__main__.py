3a1a01a33c1a5d19cccbe3579bb0e7f290477a72
DirectiveAthena <directive.athena@gmail.com>
2022-03-30 17:13:36 +0200

    Fix: emptied main

--- src/AthenaColor/__main__.py
+++ src/AthenaColor/__main__.py
@@ -6,22 +6,9 @@
 # Custom Library
 
 # Custom Packages
-from AthenaColor.Objects.ColorSystems.Opaque import (
-    RGB,
-    HSV,
-    HSL,
-    HEX,
-    CMYK
-)
 
 # ----------------------------------------------------------------------------------------------------------------------
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
-
-    rgb = RGB(165,42,42)
-    hex_ = HEX("#A52A2A")
-    print(repr(rgb))
-    print(repr(hex_))
-
-    print(RGB(189,183,107)==HEX("#BDB76D"))
+    pass
