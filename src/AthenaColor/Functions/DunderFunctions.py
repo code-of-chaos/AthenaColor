ff7e8efb6a947fcc2ab7fc794cb91ba38e0813cb
DirectiveAthena <directive.athena@gmail.com>
2022-04-18 20:21:59 +0200

    Fix: Strict zipping for dunder functions

--- src/AthenaColor/Functions/DunderFunctions.py
+++ src/AthenaColor/Functions/DunderFunctions.py
@@ -21,38 +21,38 @@ __all__ = [
 def add(left:tuple, right:tuple)->tuple:
     return tuple(
         lr[0] + lr[1]
-        for lr in zip(left, right)
+        for lr in zip(left, right, strict=True)
     )
 def sub(left:tuple, right:tuple)->tuple:
     return tuple(
         lr[0] - lr[1]
-        for lr in zip(left, right)
+        for lr in zip(left, right, strict=True)
     )
 def mul(left:tuple, right:tuple)->tuple:
     return tuple(
         lr[0] * lr[1]
-        for lr in zip(left, right)
+        for lr in zip(left, right, strict=True)
     )
 def floordiv(left:tuple, right:tuple)->tuple:
     return tuple(
         lr[0] // lr[1]
-        for lr in zip(left, right)
+        for lr in zip(left, right, strict=True)
     )
 def truediv(left:tuple, right:tuple)->tuple:
     return tuple(
         lr[0] / lr[1]
-        for lr in zip(left, right)
+        for lr in zip(left, right, strict=True)
     )
 def mod(left:tuple, right:tuple)->tuple:
     return tuple(
         lr[0] % lr[1]
-        for lr in zip(left, right)
+        for lr in zip(left, right, strict=True)
     )
 # noinspection PyShadowingBuiltins
 def pow(left:tuple, right:tuple)->tuple:
     return tuple(
         lr[0] ** lr[1]
-        for lr in zip(left, right)
+        for lr in zip(left, right, strict=True)
     )
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -61,30 +61,30 @@ def pow(left:tuple, right:tuple)->tuple:
 def gt(left:tuple,right:tuple)->bool:
     return all(
         lr[0] > lr[1]
-        for lr in zip(left, right)
+        for lr in zip(left, right, strict=True)
     )
 def lt(left:tuple,right:tuple)->bool:
     return all(
         lr[0] < lr[1]
-        for lr in zip(left, right)
+        for lr in zip(left, right, strict=True)
     )
 def eq(left:tuple,right:tuple)->bool:
     return all(
         lr[0] == lr[1]
-        for lr in zip(left, right)
+        for lr in zip(left, right, strict=True)
     )
 def ne(left:tuple,right:tuple)->bool:
     return all(
         lr[0] != lr[1]
-        for lr in zip(left, right)
+        for lr in zip(left, right, strict=True)
     )
 def le(left:tuple,right:tuple)->bool:
     return all(
         lr[0] <= lr[1]
-        for lr in zip(left, right)
+        for lr in zip(left, right, strict=True)
     )
 def ge(left:tuple,right:tuple)->bool:
     return all(
         lr[0] >= lr[1]
-        for lr in zip(left, right)
+        for lr in zip(left, right, strict=True)
     )
