28638d12676cc2fd524746bcd968c720f4fbd854
DirectiveAthena <directive.athena@gmail.com>
2022-05-02 15:29:27 +0200

    Feature: ValueError if max is less than min

--- src/AthenaColor/Functions/Constraints.py
+++ src/AthenaColor/Functions/Constraints.py
@@ -20,10 +20,12 @@ __all__ = [
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
 def Constrain(value:int|float, maximum:int|float, minimum:int|float=0) -> int|float:
+    if maximum < minimum:
+        raise ValueError(f"maximum={maximum} cannot be smaller than minimum={minimum}")
     return max(min(value, maximum),minimum)
 
 # ----------------------------------------------------------------------------------------------------------------------
-# - Constraints for OPAQUE COLORSs -
+# - Constraints for COLORS -
 # ----------------------------------------------------------------------------------------------------------------------
 def ConstrainHSV(h:int|float,s:int|float,v:int|float)->Tuple[int|float,int|float,int|float]:
     return Constrain(h, 360),Constrain(s,1),Constrain(v,1)
@@ -37,8 +39,5 @@ def ConstrainRGB(r:int,g:int,b:int)->Tuple[int,int,int]:
 def ConstrainCMYK(c:int|float,m:int|float,y:int|float,k:int|float)->Tuple[int|float,int|float,int|float,int|float]:
     return Constrain(c,1),Constrain(m,1),Constrain(y,1),Constrain(k,1)
 
-# ----------------------------------------------------------------------------------------------------------------------
-# - Constraints for TRANSPARENT COLORSs -
-# ----------------------------------------------------------------------------------------------------------------------
 def ConstrainRGBA(r:int,g:int,b:int,a:int)->Tuple[int,int,int,int]:
     return Constrain(r, 255),Constrain(g, 255),Constrain(b, 255),Constrain(a, 255)
