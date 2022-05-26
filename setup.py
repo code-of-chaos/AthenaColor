2ff890a8e3fd4ae5bbbf4389cdb5c8528ae55edd
DirectiveAthena <directive.athena@gmail.com>
2022-05-26 12:52:32 +0200

    Change: Version String

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="AthenaColor",
-    version="4.2.0",
+    version="4.2.1",
     author="Andreas Sas",
     author_email="",
     description="Package to support full usage of RGB colors in the Console.",
