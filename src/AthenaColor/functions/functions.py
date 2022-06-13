67fe8eed5ac65074d4704194d3e5cfdb11c9185f
Andreas Sas <directive.athena@gmail.com>
2022-06-13 15:15:48 +0100

    Update functions.py

--- src/AthenaColor/functions/functions.py
+++ src/AthenaColor/functions/functions.py
@@ -26,4 +26,4 @@ def round_half_up(value:int|float) -> int: # because Twidi didn't like RoundCorr
 def fix_console():
     # Apply a quick fix to make
     if sys.platform == 'win32':
-        os.system("models/Color")
+        os.system("color")
