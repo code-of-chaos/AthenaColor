b3ac11ea481d147d3edad33492ab5d8eff8d73fb
DirectiveAthena <directive.athena@gmail.com>
2022-08-30 18:35:22 +0200

    VERSION BUMP

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme_handler() -> str:
 
 def version_handler() -> str:
     # ------------------------------------------------------------------------------------------------------------------
-    version = 6,2,0 # <-- DEFINE THE VERSION IN A TUPLE FORMAT HERE
+    version = 7,0,0 # <-- DEFINE THE VERSION IN A TUPLE FORMAT HERE
     # ------------------------------------------------------------------------------------------------------------------
     version_str = ".".join(str(i) for i in version)
 
