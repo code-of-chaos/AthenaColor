6944bda811e4e0ce3f4e2938ec9c7f642106da20
DirectiveAthena <directive.athena@gmail.com>
2022-08-30 18:35:06 +0200

    Change: reformatted init file

--- src/AthenaColor/__init__.py
+++ src/AthenaColor/__init__.py
@@ -8,16 +8,34 @@ __all__ = [
 # ----------------------------------------------------------------------------------------------------------------------
 # - Package Imports -
 # ----------------------------------------------------------------------------------------------------------------------
+# General Packages
+import os
+import sys
+
+# Custom Library
+
+# Custom Packages
 import AthenaColor.data.colors_html as HtmlColorObjects
 from AthenaColor.data.style import Style, StyleNest, Basic, BasicNest
-from AthenaColor.data.bodies import Fore, ForeNest, Back, BackNest, Underline, UnderlineNest
+from AthenaColor.models.rgb_controlled import RgbControlled
+from AthenaColor.models.rgb_controlled_nested import RgbControlledNested
+
+# ----------------------------------------------------------------------------------------------------------------------
+# - Support Code -
+# ----------------------------------------------------------------------------------------------------------------------
+# Applies a quick fix to make color appear in console
+if sys.platform == 'win32':
+    os.system("color")
 
 # ----------------------------------------------------------------------------------------------------------------------
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
-import os
-import sys
+# Collection of both inline and nested objects
+#   These can be used to use color in the console
+Fore = RgbControlled(param_code=f"38;2;",)
+Back = RgbControlled(param_code=f"48;2;",)
+Underline = RgbControlled(param_code=f"58;2;",)
 
-# Applies a quick fix to make color appear in console
-if sys.platform == 'win32':
-    os.system("color")
+ForeNest = RgbControlledNested(inline_class=Fore, reset=Style.NoForeground)
+BackNest = RgbControlledNested(inline_class=Back,reset=Style.NoBackground)
+UnderlineNest = RgbControlledNested(inline_class=Underline,reset=Style.NoUnderline)
