5649c8b6062dd825f0d7472084ceb42b5872e940
DirectiveAthena <directive.athena@gmail.com>
2023-03-11 19:13:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme_handler() -> str:
 
 def version_handler() -> str:
     # ------------------------------------------------------------------------------------------------------------------
-    version = 7,0,1 # <-- DEFINE THE VERSION IN A TUPLE FORMAT HERE
+    version = 7,0,2 # <-- DEFINE THE VERSION IN A TUPLE FORMAT HERE
     # ------------------------------------------------------------------------------------------------------------------
     version_str = ".".join(str(i) for i in version)
 
